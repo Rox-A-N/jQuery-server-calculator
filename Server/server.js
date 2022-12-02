a471605ce51ea5debd34edcd82d248a6f0f39a8d
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2022-12-02 00:02:19 -0600

    added buttons to index.html and finished setting up the server

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');     // loads express
 
-const app = express();
-const PORT = 5001;
+const app = express();      // created an app instance of express
+const PORT = 5001;          // spinning up a port
 
-app.use(express.static('server/public'));
+app.use(express.static('server/public'));   // app will utilize the files in the public folder
 
-app.get()
+app.post('/operations', (req, res) => {
+    res.sendStatus(201);  // 201 means created
+});     
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+});     // spin up the server and keep it open
+        // anonymous function
--- server/public/client.js
+++ server/public/client.js
@@ -5,8 +5,8 @@ $(onReady);
 function onReady() {
     $.ajax({
         method: 'POST', //using ajax to POST (send) info to the server
-        url: '/' //this is the url path where the data is being retrieved from on the server. We wait for the response from the server...
+        url: '/operations', //this is the url path where the data is being retrieved from on the server. We wait for the response from the server...
     }).then(function (response){
-
+        console.log('post request response:', response);
     });
 }
