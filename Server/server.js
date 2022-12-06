0cf233ae1d467c41a087218cfd288538459becdb
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2022-12-05 21:31:51 -0600

    created object-equation, added some server-side logic

--- Server/server.js
+++ Server/server.js
@@ -4,12 +4,23 @@ const app = express();      // created an app instance of express
 const PORT = 5001;          // spinning up a port
 
 app.use(express.static('server/public'));   // app will utilize the files in the public folder
+app.use(express.urlencoded());
+
+app.listen(PORT, () => {   
+    console.log('listening on port', PORT);
+});     // spin up the server and keep it open
+// anonymous function
+
+
 
 app.post('/operations', (req, res) => {
+    // console.log(req.body);
+    if (req.body.operand === 'add') {
+        sum = Number(req.body.num1) + Number(req.body.num2);
+        console.log(sum);
+    }
+
     res.sendStatus(201);  // 201 means created
 });     
 
-app.listen(PORT, () => {
-    console.log('listening on port', PORT);
-});     // spin up the server and keep it open
-        // anonymous function
+
--- server/public/client.js
+++ server/public/client.js
@@ -3,10 +3,45 @@ console.log('This will be my Calculator!');
 $(onReady);
 
 function onReady() {
+    $('#addition').on('click', add);
+    $('#subtract').on('click', subtract);
+    $('#multiply').on('click', multiply);
+    $('#divide').on('click', divide);   
+    $('#submit').on('click', submit);
+}
+
+
+let equation = {
+    operand : "",
+    num1: 0,
+    num2: 0
+// these are global because they need to be accessed by multiple functions
+}
+
+function submit() {
+    let input1 = Number($('#num1').val());  // Number is a js built-in to convert to a number, otherwise it is a string. Problems when adding
+    let input2 = Number($('#num2').val());
+
+    equation.num1 = input1;     // accessing the object(key: value) pair
+    equation.num2 = input2;
+
+    console.log('This is the object ', equation);
     $.ajax({
         method: 'POST', //using ajax to POST (send) info to the server
         url: '/operations', //this is the url path where the data is being retrieved from on the server. We wait for the response from the server...
+        data: equation,     // accessing the object equation
     }).then(function (response){
         console.log('post request response:', response);
     });
-}
+}   // end submit
+
+function add() {
+    equation.operand = 'add';
+    console.log('Is it adding ', equation);  
+} // end add
+
+function subtract() {
+    equation.operand = 'subtract'
+}   // end subtract
+
+
